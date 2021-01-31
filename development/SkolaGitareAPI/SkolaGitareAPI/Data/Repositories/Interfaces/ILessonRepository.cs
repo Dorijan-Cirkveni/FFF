82452ef738d67b40e705200f719ebe147ab8289c
Renato Uštar <renato.ustar@uprise.hr>
2021-01-31 23:51:51 +0100

    BUGFIX - Get lessons

--- development/SkolaGitareAPI/SkolaGitareAPI/Data/Repositories/Interfaces/ILessonRepository.cs
+++ development/SkolaGitareAPI/SkolaGitareAPI/Data/Repositories/Interfaces/ILessonRepository.cs
@@ -1,4 +1,5 @@
-﻿using SkolaGitareAPI.Data.Entities;
+﻿using SkolaGitareAPI.Data.DTOs;
+using SkolaGitareAPI.Data.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,9 @@ namespace SkolaGitareAPI.Data.Repositories.Interfaces
 {
     public interface ILessonRepository : IGeneralRepository<Lesson>
     {
+        public Task<List<LessonDTO>> GetLessons();
+
+        public Task<LessonDTO> GetLesson(Guid id);
         public Task<IEnumerable<Lesson>> GetStudentLessons(object studentId);
 
         Task<List<Lesson>> GetEarliestLessonForStudentsInNextAppointment();
