ac18cc846c660ca7447d49e3c1042a28b9024db4
Dorijan Cirkveni <dorijan.cirkveni@engineer.com>
2021-01-31 23:56:36 +0100

    I give up.
    I'm not going to magically develop app linking skills at midnight.

--- Desktop/Assets/DataCarryingSwitchable.cs
+++ Desktop/Assets/DataCarryingSwitchable.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DataCarryingSwitchable : ISwitchable
 {
+    
+    public List<InputField> IFLD;
+    
 }
